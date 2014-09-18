8447bb322b459a08522b371f37b94269689e0577
kurtis mulgrew <kmulgrew@hotmail.com>
2014-09-18 12:39:04 +0100

    new pages

---
+++ WebServer/src/com/PotatoServer/Models/Management.java
@@ -0,0 +1,16 @@
+package com.PotatoServer.Models;
+
+import javax.sql.DataSource;
+
+public class Management {
+	
+	private DataSource _ds = null;
+
+	public void setDatasource(DataSource _ds){
+		this._ds=_ds;
+		System.out.println("Set Data Source in Model"+_ds.toString());
+		
+	}
+	
+	public void 
+}
--- WebServer/src/com/PotatoServer/Servlets/Manament.java
+++ WebServer/src/com/PotatoServer/Servlets/Manament.java
@@ -1,7 +1,11 @@
 package com.PotatoServer.Servlets;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
@@ -12,6 +16,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import com.PotatoServer.lib.Dbutils;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
 
 /**
  * Servlet implementation class Manament
@@ -39,7 +46,7 @@ public class Manament extends HttpServlet {
 		// TODO Auto-generated method stub
 		Dbutils db = new Dbutils();
 		db.createSchema();
-	
+		
         _ds=db.assemble(config);
 	}
 
@@ -48,6 +55,51 @@ public class Manament extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs= null;
+
+		String PID = request.getParameter("P_ID");
+    	
+    	
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        	con = DriverManager.getConnection("");
+            ps = con.prepareStatement(stmt);
+        	ps.setString(1, faultId);
+        	
+        	System.out.println(faultId);
+        	
+        	
+            rs=ps.executeQuery();
+            
+            if (rs.next()) {
+                // redirect to error page
+            	String PName = rs.getString("Name");
+            	String PDescription = rs.getString("Description");
+                
+            	
+            	request.setAttribute("faultDetails", faultDetails);
+            	request.setAttribute("faultSummary", faultSummary);
+            	
+            	RequestDispatcher rd = request.getRequestDispatcher("/FaultSummary.jsp"); 
+
+        		rd.forward(request, response);
+                //response.sendRedirect("/FaultDetails.jsp"); 
+                
+               
+            } 
+        } catch (Exception e)
+        {
+        System.out.println(e);
+        }
+        
+        try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
