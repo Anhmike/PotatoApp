40f6f6efa6ab5d8b5e6ebe58acc1aaf0a51845db
tbutterwith <tbutterwith@gmail.com>
2014-09-18 10:36:06 +0100

    Established connection to database called potatodb

--- WebServer/src/com/PotatoServer/Servlets/Manament.java
+++ WebServer/src/com/PotatoServer/Servlets/Manament.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,27 +14,27 @@ import javax.sql.DataSource;
 import com.PotatoServer.lib.Dbutils;
 
 /**
- * Servlet implementation class UpdatePhone
+ * Servlet implementation class Manament
  */
 @WebServlet(urlPatterns = { 
-				"/UpdatePhone", 
-				"/UpdatePhone/*"
-		}, 
-		initParams = { 
-			@WebInitParam(name = "data-source", value = "jdbc/potatodb")
-		})
-public class UpdatePhone extends HttpServlet {
+		"/Management", 
+		"/Management/*"
+}, 
+initParams = { 
+	@WebInitParam(name = "data-source", value = "jdbc/potatodb")
+})
+public class Manament extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private DataSource _ds = null;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdatePhone() {
+    public Manament() {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
     public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 		Dbutils db = new Dbutils();
@@ -42,12 +42,12 @@ public class UpdatePhone extends HttpServlet {
 	
         _ds=db.assemble(config);
 	}
-    
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// TODO Auto-generated method stub
 	}
 
 	/**
--- WebServer/src/com/PotatoServer/lib/Dbutils.java
+++ WebServer/src/com/PotatoServer/lib/Dbutils.java
@@ -1,6 +1,5 @@
 package com.PotatoServer.lib;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,8 +13,6 @@ import javax.naming.NamingException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.sql.DataSource;
-
-import com.mysql.jdbc.Statement;
 
 
 
@@ -38,14 +35,14 @@ public class Dbutils {
 			System.out.println("JNDI failure: " + ex);
 		}
 	}
-	
+
 	/**
 	 * Assembles  a DataSource from JNDI.
 	 */
 	 public DataSource assemble(ServletConfig config) throws ServletException {
 		DataSource _ds = null;
 		String dataSourceName = config.getInitParameter("data-source");
-		System.out.println("Data Source Parameter " + dataSourceName);
+		System.out.println("Data Source Parameter" + dataSourceName);
 		if (dataSourceName == null)
 			throw new ServletException("data-source must be specified");
 		Context envContext = null;
@@ -84,7 +81,8 @@ public class Dbutils {
 			return;
 		}
 		String sqlQuery = "CREATE TABLE IF NOT EXISTS `author` ("
-				+ "`idauthor` INT NOT NULL AUTO_INCREMENT," + "`name` VARCHAR(45) NULL,"
+				+ "`idauthor` INT NOT NULL AUTO_INCREMENT," + "`forename` VARCHAR(45) NULL,"+ "`surname` VARCHAR(45) NULL,"+ "`email` VARCHAR(45) NULL,"
+				+ "`pass` VARCHAR(45) NULL,"+ "`privilege` VARCHAR(45) NULL,"+ "`idfault` INT(11) NULL,"
 				+ "PRIMARY KEY (`idauthor`))" + "ENGINE = InnoDB;";
 		try {
 			pmst = Conn.prepareStatement(sqlQuery);
@@ -107,18 +105,18 @@ public class Dbutils {
 		sqlQuery = "CREATE TABLE IF NOT EXISTS `fault` ("
 				+ "`idfault` INT NOT NULL AUTO_INCREMENT," + "`summary` VARCHAR(45) NULL,"
 				+ "`details` VARCHAR(100) NULL,"
-				+ "`author_idauthor` INT NOT NULL,"
-				+ "`section_idsection` INT NOT NULL,"
+				+ "`author_idauthor` INT(11) NULL,"
+				+ "`section_idsection` INT(11) NULL,"
 				+ "PRIMARY KEY (`idfault`),"
 				+ "INDEX `fk_fault_author_idx` (`author_idauthor` ASC),"
 				+ "INDEX `fk_fault_section1_idx` (`section_idsection` ASC),"
 				+ "CONSTRAINT `fk_fault_author`"
 				+ "  FOREIGN KEY (`author_idauthor`)"
 				+ " REFERENCES `author` (`idauthor`)"
-				+ "ON DELETE NO ACTION" + " ON UPDATE NO ACTION,"
+				+ "ON DELETE SET NULL" + " ON UPDATE SET NULL,"
 				+ "CONSTRAINT `fk_fault_section1`"
 				+ " FOREIGN KEY (`section_idsection`)"
-				+ "REFERENCES `section` (`idsection`)" + "ON DELETE NO ACTION "
+				+ "REFERENCES `section` (`idsection`)" + "ON DELETE SET NULL "
 				+ "ON UPDATE NO ACTION)" + "ENGINE = InnoDB;";
 		try {
 			pmst = Conn.prepareStatement(sqlQuery);
@@ -128,7 +126,7 @@ public class Dbutils {
 			return;
 		}
 		ResultSet rs=null;
-		sqlQuery="Select count(name) from author as rowcount";
+		sqlQuery="Select count(forename) from author as rowcount";
 		try {
 			pmst = Conn.prepareStatement(sqlQuery);
 			rs=pmst.executeQuery();
@@ -136,7 +134,7 @@ public class Dbutils {
 			    int rows = rs.getInt(1);
 			    System.out.println("Number of Rows " + rows);
 			    if (rows==0){
-			    	sqlQuery="INSERT INTO `author` (`name`) VALUES ('Andy'),('Tracey'),('Tom'),('Bill');";
+			    	sqlQuery="INSERT INTO `author` (`forename`,`surname`,`email`,`pass`,`privilege`,`idfault`) VALUES ('Andy','Cobley','a.e.cobley@dundee.ac.uk','password','Admin','1'), ('Tracey','Trent','t@email.com','password','Developer','2'),('Tom','Hanks','tomhanks@email.com','password','Reporter','2'),('Bill','Clinton','b@email.com','password','Reporter','2');";
 					try {
 						pmst = Conn.prepareStatement(sqlQuery);
 						pmst.executeUpdate();
@@ -152,7 +150,7 @@ public class Dbutils {
 						System.out.println("Can not insert names in sections "+ex);
 						return;	
 					}
-					sqlQuery="INSERT INTO `fault` (`summary`,`details`,`author_idauthor`,`section_idsection`) VALUES ('Startup fails on a pi','Because the number of processors returned is zero startup fails','1','1');";
+					sqlQuery="INSERT INTO `fault` (`summary`,`details`,`author_idauthor`,`section_idsection`) VALUES ('Startup fails on a pi','Because the number of processors returned is zero startup fails','1','1'),('My screen is black','I dont know why?','2','2'),('Its broken!!','Ive just turned it on and it broke???','3','3');";
 					try {
 						pmst = Conn.prepareStatement(sqlQuery);
 						pmst.executeUpdate();
@@ -162,29 +160,27 @@ public class Dbutils {
 					}
 			    }
 			}
-			
+
 		} catch (Exception ex) {
 			System.out.println("Can not select count "+ex);
 			return;
 		}
-		
-	
  
-		
+
 	}
 	
 	public void createSchema(){
-		String url = "jdbc:mysql://localhost";											///DATABASE URL
+		String url = "jdbc:mysql://localhost";
 		Connection conn=null;
 		try {
 		   Class.forName ("com.mysql.jdbc.Driver").newInstance ();
-		   conn = DriverManager.getConnection (url, "root", "Cl1m8t3;");				///USERNAME AND PASSWORD
-	
+		   conn = DriverManager.getConnection (url, "potato", "potato");  
+
 		}catch (Exception et){
 			System.out.println("Can't get conenction to create schema "+et);
 			return;
 		}
-		String sqlcreateSchema="Create database if not exists faultdb ;";
+		String sqlcreateSchema="Create database if not exists kfaultdb ;";
 		try{
 			java.sql.Statement statement=conn.createStatement();
 			statement.execute(sqlcreateSchema);
@@ -193,6 +189,6 @@ public class Dbutils {
 			System.out.println("Can not create schema ");
 			return;
 		}
-		
+
 	}
 }
