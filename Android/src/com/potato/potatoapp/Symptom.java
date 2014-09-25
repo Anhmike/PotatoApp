99ad6cf5fc1f0d37a7507dc44edffa4d8c5a165e
ronsch <ron.schoenberg@gmail.com>
2014-09-25 13:08:03 +0100

    NEw files

---
+++ Android/src/com/potato/potatoapp/Symptom.java
@@ -0,0 +1,29 @@
+package com.example.potatodiagnosisstatic;
+
+public class Symptom {
+	int symID;
+	String symName;
+	int symParent;
+	public Symptom(){
+		
+	}
+	public int getSymID() {
+		return symID;
+	}
+	public String getSymName() {
+		return symName;
+	}
+	public int getSymParent() {
+		return symParent;
+	}
+	public void setSymID(int symID) {
+		this.symID = symID;
+	}
+	public void setSymName(String symName) {
+		this.symName = symName;
+	}
+	public void setSymParent(int symParent) {
+		this.symParent = symParent;
+	}
+	
+}
