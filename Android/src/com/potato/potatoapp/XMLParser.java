e6ae0b3546aba113cd5de8786a3c237a025f6b45
tbutterwith <tbutterwith@gmail.com>
2014-09-25 13:12:51 +0100

    Removed static app from main app folder

---
+++ Android/src/com/potato/potatoapp/XMLParser.java
@@ -0,0 +1,16 @@
+package com.potato.potatoapp;
+
+import java.util.ArrayList;
+
+public class XMLParser {
+	
+	public XMLParser () {
+		
+	}
+	
+	public static ArrayList<ArrayList<Object>> parseXML(String feed) {
+		
+		return null;
+	}
+
+}
